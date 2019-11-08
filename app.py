3e87fafea3847192aee48a2ad216cecdc6765f56
Yafei Guo <yafei-guo@uiowa.edu>
2019-11-08 10:01:04 -0600

    modified

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = conn
 db = SQLAlchemy(app)
 
 class yafguo_baseballplayersapp(db.Model):
-    #__tablename__ = 'results'
     playerid = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(255))
     last_name = db.Column(db.String(255))
