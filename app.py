2a900af2439c3df4ca6fde29da29f259b3fb2a69
Yafei Guo <yafei-guo@uiowa.edu>
2019-11-07 22:26:22 -0600

    Delete a record

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, request, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
@@ -8,42 +8,57 @@ import pymysql
 import secrets
 
 conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
+#conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(dbuser, dbpass, dbhost, dbname)
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='SuperSecretKry'
+app.config['SECRET_KEY']='SuperSecretKey'
 app.config['SQLALCHEMY_DATABASE_URI'] = conn
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 
 class yafguo_baseballplayersapp(db.Model):
+    #__tablename__ = 'results'
     playerid = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(255))
     last_name = db.Column(db.String(255))
 
     def __repr__(self):
-        return "id: {0} | first name: {1} | last name: {2}".format(self.playerid, self.first_name, self.last_name)
+        return "id: {0} | first name: {1} | last name: {2}".format(self.id, self.first_name, self.last_name)
 
-class playerForm(FlaskForm):
+class PlayerForm(FlaskForm):
     first_name = StringField('First Name:', validators=[DataRequired()])
     last_name = StringField('Last Name:', validators=[DataRequired()])
+
 
 @app.route('/')
 def index():
     all_players = yafguo_baseballplayersapp.query.all()
-    return render_template('index.html', players=all_players, pageTitle='Baseball Players')
+    return render_template('index.html', players=all_players, pageTitle='Baseball players')
 
 @app.route('/add_player', methods=['GET', 'POST'])
 def add_player():
-    form = playerForm()
+    form = PlayerForm()
     if form.validate_on_submit():
         player = yafguo_baseballplayersapp(first_name=form.first_name.data, last_name=form.last_name.data)
         db.session.add(player)
         db.session.commit()
-        return "<h2> The baseball player is {0} {1}".format(form.first_name.data, form.last_name.data)
+        return redirect('/')
 
-    return render_template('add_player.html', form=form, pageTitle='Add A New Friend')
+    return render_template('add_player.html', form=form, pageTitle='Add A New Player')
 
 
-    if __name__=='__main__':
-        app.run(debug=True)
+@app.route('/delete_player/<int:playerid>', methods=['GET','POST'])
+def delete_player(playerid):
+    if request.method == 'POST': #if it's a POST request, delete the friend from the database
+        obj = yafguo_baseballplayersapp.query.filter_by(playerid=playerid).first()
+        db.session.delete(obj)
+        db.session.commit()
+        flash('Player was successfully deleted!')
+        return redirect("/")
+
+    else: #if it's a GET request, send them to the home page
+        return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
