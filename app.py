f4891a77bdcc324efa3aa60aa346a8124bd965ff
Yafei Guo <yafei-guo@uiowa.edu>
2019-11-18 12:08:32 -0600

    Updated

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from flask import render_template, redirect, request, flash
+from flask import render_template, redirect, request, flash, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_
 import pymysql
 #import secrets
 import os
@@ -31,6 +32,7 @@ class yafguo_baseballplayersapp(db.Model):
         return "id: {0} | first name: {1} | last name: {2}".format(self.id, self.first_name, self.last_name)
 
 class PlayerForm(FlaskForm):
+    playerid = IntegerField('Player ID:')
     first_name = StringField('First Name:', validators=[DataRequired()])
     last_name = StringField('Last Name:', validators=[DataRequired()])
 
@@ -39,6 +41,18 @@ class PlayerForm(FlaskForm):
 def index():
     all_players = yafguo_baseballplayersapp.query.all()
     return render_template('index.html', players=all_players, pageTitle='Baseball players')
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        form = request.form
+        search_value = form['search_string']
+        search = "%{}%".format(search_value)
+        results = yafguo_baseballplayersapp.query.filter(or_(yafguo_baseballplayersapp.first_name.like(search),yafguo_baseballplayersapp.last_name.like(search))).all()
+        return render_template('index.html', players=results,pageTitle='Baseball Player', legend="Search Results")
+    else:
+        return redirect('/')
+
 
 @app.route('/add_player', methods=['GET', 'POST'])
 def add_player():
@@ -52,17 +66,38 @@ def add_player():
     return render_template('add_player.html', form=form, pageTitle='Add A New Player')
 
 
-@app.route('/delete_player/<int:playerid>', methods=['GET','POST'])
-def delete_player(playerid):
+@app.route('/delete_player/<int:player_id>', methods=['GET','POST'])
+def delete_player(player_id):
     if request.method == 'POST': #if it's a POST request, delete the friend from the database
-        obj = yafguo_baseballplayersapp.query.filter_by(playerid=playerid).first()
-        db.session.delete(obj)
+        player = yafguo_baseballplayersapp.query.get_or_404(player_id)
+        db.session.delete(player)
         db.session.commit()
-        flash('Player was successfully deleted!')
         return redirect("/")
 
     else: #if it's a GET request, send them to the home page
         return redirect("/")
+
+@app.route('/player/<int:player_id>', methods=['GET','POST'])
+def get_player(player_id):
+    player = yafguo_baseballplayersapp.query.get_or_404(player_id)
+    return render_template('player.html', form=player, pageTitle='Player Details', legend="Player Details")
+
+@app.route('/player/<int:player_id>/update', methods=['GET','POST'])
+def update_player(player_id):
+    player = yafguo_baseballplayersapp.query.get_or_404(player_id)
+    form = PlayerForm()
+
+    if form.validate_on_submit():
+        player.first_name = form.first_name.data
+        player.last_name = form.last_name.data
+        db.session.commit()
+        return redirect(url_for('get_player', player_id=player.playerid))
+    #elif request.method == 'GET':
+    form.playerid.data=player.playerid
+    form.first_name.data = player.first_name
+    form.last_name.data = player.last_name
+    return render_template('add_player.html', form=form, pageTitle='Update Post',
+                            legend="Update A Player")
 
 
 if __name__ == '__main__':
